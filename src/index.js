fef70ece5ca4d1844e3fb9fd72725ef6865ecf8b
Tim Suchanek <tim.suchanek@gmail.com>
2017-04-08 21:52:09 +0200

    rm src

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import AsyncComponentProvider from './AsyncComponentProvider'
-import createAsyncContext from './createAsyncContext'
-import asyncComponent from './asyncComponent'
-
-export { AsyncComponentProvider, createAsyncContext, asyncComponent }
